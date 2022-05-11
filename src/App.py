7dccfc576b450b1a1d5ca8146626821e14ecd671
marcrusch <marc.rusch@hotmail.ch>
2022-05-11 10:23:54 +0200

    Change function name

--- src/App.py
+++ src/App.py
@@ -2,9 +2,9 @@ from datetime import datetime
 import asyncio
 
 
-async def foo():
+async def clock():
     while True:
         await asyncio.sleep(1)
         pyscript.write("timeOutput", datetime.now().strftime('%H:%M:%S'))
 
-pyscript.run_until_complete(foo())
+pyscript.run_until_complete(clock())
